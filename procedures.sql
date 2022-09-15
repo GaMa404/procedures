9f21cdcacf8ed596919ed38f9e57d7ee91f99b57
GaMa404 <luzettileonardo@gmail.com>
2022-09-15 15:37:51 -0300

    first commit

---
+++ procedures.sql
@@ -0,0 +1,17 @@
+DELIMITER //
+CREATE PROCEDURE insere_daods()
+BEGIN
+	DECLARE erro_sql TINYINT DEFAULT FALSE;
+    DECLARE CONTINUE HANDLER FOR SQLEXCEPTION SET erro_sql = TRUE;
+START TRANSACTION;
+	INSERT INTO dados_livro (nome, isbn13, preco) VALUES ('História do Brasil', '1234567890123', 78.90);
+    INSERT INTO dados_livro (nome, isbn13, preco) VALUES ('Química Experimental', '9876543210987', 85.79);
+    INSERT INTO dados_livro (nome, isbn13, preco) VALUES ('História do Brasil', '1324659173820', 25,80); #ERRO
+    IF erro_sql = FALSE THEN
+		COMMIT;
+        SELECT 'Transação efetivada com sucesso.' AS resultado;
+    ELSE
+		ROLLBACK;
+        SELECT 'Erro na transação!' AS resultado;
+	END IF;
+END //
